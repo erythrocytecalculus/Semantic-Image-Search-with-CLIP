956c0323c4eee46ae9fe952b5fc9cb9321107b6a
Amilin Shahida <amilin670@gmail.com>
2026-02-18 22:51:53 +0100

    Creates the interactive UMAP plot and adds the hover effect

--- umap_interactive.py
+++ umap_interactive.py
@@ -17,3 +17,28 @@ umap_embeddings = reducer.fit_transform(embeddings)
 image_folder = r"C:\Users\TUFA17\OneDrive\Desktop\Semantische-Bildsuche-mit-CLIP\OpenImages\resize"
 filenames = os.listdir(image_folder)
 
+# Convert images to base64 for hover
+def img_to_base64(path, size=(128, 128)):
+    img = Image.open(path).convert("RGB").resize(size)
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    return base64.b64encode(buffer.getvalue()).decode()
+
+hover_images = [
+    img_to_base64(os.path.join(image_folder, f))
+    for f in filenames[:len(umap_embeddings)]
+]
+
+fig = px.scatter(
+    x=umap_embeddings[:, 0],
+    y=umap_embeddings[:, 1],
+    title="Interactive UMAP of CLIP Image Embeddings",
+)
+
+fig.update_traces(
+    marker=dict(size=6, opacity=0.7),
+    hovertemplate='<img src="data:image/png;base64,%{customdata}" width="128"><extra></extra>',
+    customdata=hover_images
+)
+
+fig.show()
