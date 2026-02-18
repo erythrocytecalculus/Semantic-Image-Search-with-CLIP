61914131b70e16a84b6bdf1c2ef1d14aeb9a8ab6
Amilin Shahida <amilin670@gmail.com>
2026-02-18 22:56:12 +0100

    Load files and file path

--- umap_thumbnails.py
+++ umap_thumbnails.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from PIL import Image
 import os
+
+# Load embeddings
+embeddings = np.load("image_embeddings.npy")
+
+# Run UMAP
+reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, metric="cosine", random_state=42)
+umap_embeddings = reducer.fit_transform(embeddings)
+
+# Image folder
+image_folder = r"C:\Users\TUFA17\OneDrive\Desktop\Semantische-Bildsuche-mit-CLIP\OpenImages\resize"
+filenames = os.listdir(image_folder)
