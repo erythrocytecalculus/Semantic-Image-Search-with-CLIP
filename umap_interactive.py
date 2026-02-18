e109a4149857354851df579cc6d30fa3a0b33aad
Amilin Shahida <amilin670@gmail.com>
2026-02-18 22:51:20 +0100

    Add file path

--- umap_interactive.py
+++ umap_interactive.py
@@ -6,3 +6,14 @@ from PIL import Image
 import base64
 from io import BytesIO
 
+# Load embeddings
+embeddings = np.load("image_embeddings.npy")
+
+# UMAP
+reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, metric="cosine", random_state=42)
+umap_embeddings = reducer.fit_transform(embeddings)
+
+# Image folder
+image_folder = r"C:\Users\TUFA17\OneDrive\Desktop\Semantische-Bildsuche-mit-CLIP\OpenImages\resize"
+filenames = os.listdir(image_folder)
+
